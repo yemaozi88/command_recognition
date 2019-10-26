944906f15e0521d0c9654ea7d15f5bd17683717c
yemaozi88 <428968@gmail.com>
2019-10-26 15:57:04 +0200

    a class is made for command recognition.

--- run.py
+++ run.py
@@ -8,40 +8,47 @@ sys.path.append(default.tensorflow_examples_speech_commands_dir)
 
 from label_wav import load_graph, load_labels
 
-# load command labels.
-labels = load_labels(default.labels_txt)
-# load graph, which is stored in the default session
-load_graph(default.graph_pb)
-input_layer_name  = 'wav_data:0'
-output_layer_name = 'labels_softmax:0'
-num_top_predictions = 3
 
-def label_wav(wav):
-    if not wav or not tf.gfile.Exists(wav):
-        tf.logging.fatal('Audio file does not exist %s', wav)
+class CommandRecognizer:
+    def __init__(self, labels_txt, graph_pb):
+        # load command labels.
+        self.labels = load_labels(labels_txt)
+        # load graph, which is stored in the default session
+        load_graph(graph_pb)
 
-    with open(wav, 'rb') as wav_file:
-        wav_data = wav_file.read()
+        # copied from temsorflow/examples/command_recognition/label_wav.py
+        self.input_layer_name  = 'wav_data:0'
+        self.output_layer_name = 'labels_softmax:0'
 
-    with tf.Session() as sess:
-        # Feed the audio data as input to the graph.
-        #   predictions  will contain a two-dimensional array, where one
-        #   dimension represents the input image count, and the other has
-        #   predictions per class
-        softmax_tensor = sess.graph.get_tensor_by_name(output_layer_name)
-        predictions,   = sess.run(softmax_tensor, {input_layer_name: wav_data})
+        # initialize variables.
+        self.predictions = []
+        self.ranking = []
 
-        # Sort to show labels in order of confidence
-        top_k = predictions.argsort()[-num_top_predictions:][::-1]
-        for node_id in top_k:
-            human_string = labels[node_id]
-            score = predictions[node_id]
-            print('%s (score = %.5f)' % (human_string, score))
+
+    def predict_labels(self, wav_data):
+        with tf.Session() as sess:
+            # Feed the audio data as input to the graph.
+            #   predictions  will contain a two-dimensional array, where one
+            #   dimension represents the input image count, and the other has
+            #   predictions per class
+            softmax_tensor = sess.graph.get_tensor_by_name(self.output_layer_name)
+            predictions, = sess.run(softmax_tensor, {self.input_layer_name: wav_data})
+            self.predictions = predictions
+            self.ranking = predictions.argsort()[:][::-1]
+            return
+
+
+    def label_wav_file(self, wav_file):
+        if not wav_file or not tf.gfile.Exists(wav_file):
+            tf.logging.fatal('wav file does not exist %s', wav_file)
+        with open(wav_file, 'rb') as wav:
+            wav_data = wav.read()
+        self.predict_labels(wav_data)
 
 
 if __name__ == '__main__':
-    #-graph = / home / aki / Data / my_frozen_graph.pb -
-    # -labels = / home / aki / Data / speech_commands_train / conv_labels.txt -
-    # -wav = / home / aki / Data / speech_dataset / left / a5d485dc_nohash_0.wav
     wav_file = r'/home/aki/Data/speech_dataset/left/a5d485dc_nohash_0.wav'
-    label_wav(wav_file)
+
+    cr = CommandRecognizer(default.labels_txt, default.graph_pb)
+    cr.label_wav_file(wav_file)
+    print('recognized as {}'.format(cr.labels[cr.ranking[0]]))
