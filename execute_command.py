8592394d8fafc3092b050ec3b389ba378698156f
yemaozi88 <428968@gmail.com>
2019-11-01 00:16:51 +0100

    fixed ev3dev.motor is used instead of ev3dev.sound when the robot should talk.

--- execute_command.py
+++ execute_command.py
@@ -3,23 +3,25 @@ import argparse
 
 import defaultfiles as default
 sys.path.append(default.ev3dev_lang_python_dir)
-import ev3dev2.motor as ev3motor
-import ev3dev2.sound as ev3sound
+import ev3dev2.motor as ev3
+from ev3dev2.sound import Sound
 
-steer = ev3motor.MoveSteering(ev3motor.OUTPUT_B, ev3motor.OUTPUT_C)
+steer = ev3.MoveSteering(ev3.OUTPUT_B, ev3.OUTPUT_C)
+sound = Sound()
+
 def execute_command(command):
     if command == 'go':
-        steer.on_for_rotations(0, ev3motor.SpeedPercent(default.speed_percent), default.rotation)
+        steer.on_for_rotations(0, ev3.SpeedPercent(default.speed_percent), default.rotation)
     elif command == 'right':
-        steer.on_for_rotations(100, ev3motor.SpeedPercent(default.speed_percent), default.rotation)
+        steer.on_for_rotations(100, ev3.SpeedPercent(default.speed_percent), default.rotation)
     elif command == 'left':
-        steer.on_for_rotations(-100, ev3motor.SpeedPercent(default.speed_percent), default.rotation)
+        steer.on_for_rotations(-100, ev3.SpeedPercent(default.speed_percent), default.rotation)
     elif command == 'down':
-        steer.on_for_rotations(0, ev3motor.SpeedPercent(default.speed_percent), -default.rotation)
+        steer.on_for_rotations(0, ev3.SpeedPercent(default.speed_percent), -default.rotation)
     elif command == 'stop':
-        ev3sound.sound.speak('stop.').wait()
+        sound.speak('stop.').wait()
     else:
-        ev3sound.sound.speak('no valid command received.').wait()
+        sound.speak('no valid command received.').wait()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
