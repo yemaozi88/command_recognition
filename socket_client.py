6325d532c6ae021973a6397f570f08049bee56e6
yemaozi88 <428968@gmail.com>
2019-11-18 21:13:33 +0100

    socket test.

--- socket_client.py
+++ socket_client.py
@@ -2,8 +2,8 @@
 
 import socket
 
-HOST = 'robot@ev3dev.local'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = 'ev3dev'  # The server's hostname or IP address
+PORT = 65432     # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
--- socket_server.py
+++ socket_server.py
@@ -2,8 +2,8 @@
 
 import socket
 
-HOST = 'robot@ev3dev.local'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = 'ev3dev'  # Standard loopback interface address (localhost)
+PORT = 65432     # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
