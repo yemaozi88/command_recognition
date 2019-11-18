32329674887e921554d1539a4af0c1ada59a21f1
yemaozi88 <428968@gmail.com>
2019-11-18 21:23:05 +0100

    socket test.

--- socket_client.py
+++ socket_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import time
 
 HOST = 'ev3dev'  # The server's hostname or IP address
 PORT = 65432     # The port used by the server
@@ -8,6 +9,10 @@ PORT = 65432     # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    time.sleep(2)
+    s.sendall(b'this is a test')
+    time.sleep(2)
+    s.sendall(b'did you hear me')
+    #data = s.recv(1024)
 
-print('Received', repr(data))
+#print('Received', repr(data))
